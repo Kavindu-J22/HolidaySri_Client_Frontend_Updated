04a53f8d960d5ad73dbaff6be64dd99a09a81cbc
Kavindu-J22 <kavindujayasinghe2002122@gmail.com>
2025-11-25 19:27:57 +0530

    up

--- src/pages/FoodsBeveragesBrowse.jsx
+++ src/pages/FoodsBeveragesBrowse.jsx
@@ -386,7 +386,7 @@ const categoryOptions = [
                     : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300'
                 }`}
               >
-                Food Products
+                Foods & Products
               </button>
               <button
                 onClick={() => setActiveTab('restaurants')}
